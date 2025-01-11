a5511620d9efa7b25cc3bed2940bcb59ad04c5d4
sharifabee <sharifabee.alur@gmail.com>
2025-01-11 12:16:31 +0530

    jan11

--- src/main/java/com/book/controller/UserController.java
+++ src/main/java/com/book/controller/UserController.java
@@ -3,6 +3,7 @@ package com.book.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.book.entity.BookUser;
 import com.book.service.UserService;
 
 @RestController
+@CrossOrigin("*")
 public class UserController {
 	
 	@Autowired
