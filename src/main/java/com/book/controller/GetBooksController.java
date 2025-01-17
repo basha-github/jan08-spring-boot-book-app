a89c65c31e49e5a1c41199552c1a8a6da6e3e540
sharifabee <sharifabee.alur@gmail.com>
2025-01-17 10:00:05 +0530

    jan17

--- src/main/java/com/book/controller/GetBooksController.java
+++ src/main/java/com/book/controller/GetBooksController.java
@@ -3,6 +3,7 @@ package com.book.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.book.entity.Book;
 import com.book.service.BookService;
 
 @RestController
+@CrossOrigin("*")
 public class GetBooksController {
 	
 	@Autowired
